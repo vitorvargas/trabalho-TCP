43a6e1b11116bbf9237a9181229695ab00a30e14
Vitor Vargas <vitor.vargas.2012@gmail.com>
2021-10-31 07:46:14 -0300

    Added MIDI SaveFile
    Algorithm now saves into MIDI format

--- trabalhoTCP/src/caractere.java
+++ trabalhoTCP/src/caractere.java
@@ -1,6 +1,11 @@
-import java.util.Scanner;
 
-import org.jfugue.Player;
+import java.util.Scanner;
+import java.util.Vector;
+
+import org.jfugue.*;
+
+
+//import org.jfugue.Player;
 
 //import javax.swing.JTextField;
 
@@ -28,45 +33,62 @@ public class caractere {
   public void validarTocarCaractere(String caractere, Player player) {
     notas notas = new notas();
     int length = caractere.length();
+    Vector<String> v= new Vector<String>(length);
+  
+    
 
     for (int i = 0; i < length; i++) {
       switch (caractere.charAt(i)) {
       case 'a':
         System.out.println("toca Nota La");
-        notas.tocarNotas("A", player);
+        v.add(i,"A"+" ");
+        notas.tocarNotas("A", player, length, i,v);
+        
         break;
 
       case 'b':
         System.out.println("tocar Nota Si");
-        notas.tocarNotas("B", player);
+        v.add(i,"B"+" ");
+        notas.tocarNotas("B", player, length, i,v);
+        
         break;
 
       case 'c':
         System.out.println("tocar Nota Do");
-        notas.tocarNotas("C", player);
+        v.add(i,"C"+" ");
+        notas.tocarNotas("C", player, length, i,v);
+        
         break;
 
       case 'd':
         System.out.println("tocar Nota Re");
-        notas.tocarNotas("D", player);
+        v.add(i,"D"+" ");
+        notas.tocarNotas("D", player, length, i,v);
+        
         break;
 
       case 'e':
         System.out.println("tocar Nota Mi");
-        notas.tocarNotas("E", player);
+        v.add(i,"E"+" ");
+        notas.tocarNotas("E", player, length, i,v);
+        
         break;
 
       case 'f':
         System.out.println("tocar Nota Fa");
-        notas.tocarNotas("F", player);
+        v.add(i,"F"+" ");
+        notas.tocarNotas("F", player, length, i,v);
+        
         break;
 
       case 'g':
         System.out.println("tocar Nota Sol");
-        notas.tocarNotas("G", player);
+        v.add(i,"G"+" ");
+        notas.tocarNotas("G", player, length, i,v);
+        
         break;
       }
     }
-
-  }
+  
+}
 }
--- trabalhoTCP/src/notas.java
+++ trabalhoTCP/src/notas.java
@@ -1,10 +1,37 @@
 import org.jfugue.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
 
 public class notas {
-   public void tocarNotas(String nota, Player player) {
+   
+   public void tocarNotas(String nota, Player player, int tamanho, int posatual, Vector<String> v) {
+      
       Pattern pattern = new Pattern(nota);
+      
       player.play(pattern);
-   }
+
+     if(((tamanho-1)-posatual)==0){
+
+     File file = new File("arquivosom.mid");
+     StringBuffer v1 = new StringBuffer();
+     String v2;
+     for(int j=0;j<tamanho;j++){
+      v1.append(v.get(j));
+     }v2=v1.toString();
+     Pattern pattern2 = new Pattern(v2);
+    
+     try {
+      player.saveMidi(pattern2, file);
+      System.out.println("arquivo eh os guri pattern2");
+  
+    } catch (IOException e) {
+      //TODO Auto-generated catch block
+      e.printStackTrace();} 
+
+  }
+}
+
 
    public void pausar() {
    }

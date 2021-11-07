10736b3b88afce6c778363c74b59b164b42046eb
Bianca Pelegrini <bmpelegrini@gmail.com>
2021-11-07 18:04:25 -0300

    implement pause/silence + formatting

--- trabalhoTCP/src/notas.java
+++ trabalhoTCP/src/notas.java
@@ -4,33 +4,40 @@ import java.io.IOException;
 import java.util.Vector;
 
 public class notas {
-   
+
    public void tocarNotas(String nota, Player player, int tamanho, int posatual, Vector<String> v) {
-      
+
       Pattern pattern = new Pattern(nota);
-      
+
       player.play(pattern);
 
-     if(((tamanho-1)-posatual)==0){
-     File file = new File("arquivosom.mid");
-     StringBuffer v1 = new StringBuffer();
-     String v2;
-     for(int j=0;j<tamanho;j++){
-      v1.append(v.get(j));
-     }v2=v1.toString();
-     Pattern pattern2 = new Pattern(v2);
-     System.out.println("arquivo salvo com sucesso");
-    
-     try {
-      player.saveMidi(pattern2, file);
-  
-    } catch (IOException e) {
-      //TODO Auto-generated catch block
-      e.printStackTrace();} 
+      if (((tamanho - 1) - posatual) == 0) {
+         File file = new File("arquivosom.mid");
+         StringBuffer v1 = new StringBuffer();
+         String v2;
+         for (int j = 0; j < tamanho; j++) {
+            v1.append(v.get(j));
+         }
+         v2 = v1.toString();
+         Pattern pattern2 = new Pattern(v2);
+         System.out.println("arquivo salvo com sucesso");
 
-  }
-}
+         try {
+            player.saveMidi(pattern2, file);
+         } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }
+
+      }
+   }
 
    public void pausar() {
+      try {
+         Thread.sleep(1000);
+      } catch (InterruptedException e) {
+         Thread.currentThread().interrupt();
+         e.printStackTrace();
+      }
    }
 }
